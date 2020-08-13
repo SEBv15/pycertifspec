11f357cea942da73a5c96f8c0a343982d4f78ae8
Sebastian <sebastian@strempfer.com>
2020-08-12 20:49:42 -0500

    Update Flags.py

--- pycertifspec/Flags.py
+++ pycertifspec/Flags.py
@@ -1,3 +1,9 @@
 class Flags:
-    # FLAGS
-    SV_DELETED = 0x1000 # not even used...
+    """
+    Building blocks for the `flags` field
+    
+    Currently only one flag is in use by SPEC
+    """
+
+    SV_DELETED = 0x1000
+    """Sent to clients when watched variables or associative array elements are deleted"""
